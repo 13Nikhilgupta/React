613ca7c7e0205e413ad53a4b28bd67f211b9dba5
Nikhil Gupta <g.hritik13@gmail.com>
2023-08-22 19:08:01 +0530

    Initial Commit

---
+++ Contact-List/src/contacts.js
@@ -0,0 +1,24 @@
+const contacts = [
+  {
+    name: "Nikhil Gupta",
+    imgURL: "https://github.com/13Nikhilgupta/My-Website/blob/main/img/dp.jpg?raw=true",
+    phone: "+91 8090703028",
+    email: "g.hritik13@gmail.com"
+  },
+  {
+    name: "G.Hritik",
+    imgURL:
+      "https://github.com/13Nikhilgupta/My-Website/blob/main/img/smile.png?raw=true",
+    phone: "+91 9876543210",
+    email: "g.hritik@me.com"
+  },
+  {
+    name: "Gupta Nikhil",
+    imgURL:
+      "https://github.com/13Nikhilgupta/My-Website/blob/main/img/happy.png?raw=true",
+    phone: "+91 0123456789",
+    email: "g.nikhil@me.com"
+  }
+];
+
+export default contacts;
---
+++ Note-It/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <div>
+            <footer><p>Copyright @ 2023</p></footer>
+        </div>
+    );
+};
+
+export default Footer;
